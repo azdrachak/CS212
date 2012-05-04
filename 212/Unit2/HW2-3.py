3a430f62b5bd90646cdc2811764f0776d9932d61
Aliaksandr Zdrachak <iscander.drake@gmail.com>
2012-05-04 12:10:54 +0300

    HW2-3

---
+++ 212/Unit2/HW2-3.py
@@ -0,0 +1,52 @@
+# --------------
+# User Instructions
+#
+# Write a function, longest_subpalindrome_slice(text) that takes 
+# a string as input and returns the i and j indices that 
+# correspond to the beginning and end indices of the longest 
+# palindrome in the string. 
+#
+# Grading Notes:
+# 
+# You will only be marked correct if your function runs 
+# efficiently enough. We will be measuring efficency by counting
+# the number of times you access each string. That count must be
+# below a certain threshold to be marked correct.
+#
+# Please do not use regular expressions to solve this quiz!
+
+import itertools as itr
+
+def is_palindrome(string):
+    return True if string == ''.join(reversed(string)) else False
+
+def longest_subpalindrome_slice(text):
+    """Return (i, j) such that text[i:j] is the longest palindrome in text."""
+    txt = str(text)
+    curr_pal = ''
+    start = 0
+    end = 0
+    for i in range(len(txt)+1):
+        for j in range(i, len(txt)+1):
+            iter_pal = txt.lower()[i: j]
+            if is_palindrome(iter_pal):
+                if len(curr_pal) < len(iter_pal):
+                    curr_pal = iter_pal
+                    start = i
+                    end = j
+    return (start, end)
+
+
+def test():
+    L = longest_subpalindrome_slice
+    assert L('racecar') == (0, 7)
+    assert L('Racecar') == (0, 7)
+    assert L('RacecarX') == (0, 7)
+    assert L('Race carr') == (7, 9)
+    assert L('') == (0, 0)
+    assert L('something rac e car going') == (8,21)
+    assert L('xxxxx') == (0, 5)
+    assert L('Mad am I ma dam.') == (0, 15)
+    return 'tests pass'
+
+print test()
